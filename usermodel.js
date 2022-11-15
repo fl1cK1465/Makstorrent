db72ab4d901c613de5d37f0469d0ca5f1e458a28
Maksat Abibullaev <abibullaevmaks147@gmail.com>
2022-11-16 02:19:38 +0600

    bb

--- usermodel.js
+++ usermodel.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     city: {type: String, required: true,},
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
     img: {type: String}
 })
 
